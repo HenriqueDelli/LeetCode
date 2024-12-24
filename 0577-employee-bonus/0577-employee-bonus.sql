146f9ee12f890d3a8b900035ef103b8681ec4a83
HenriqueDelli <134344871+HenriqueDelli@users.noreply.github.com>
2024-12-24 00:46:04 -0300

    Time: 1908 ms (5.59%), Space: 0B (100.00%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,15 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    A.name,
+    B.bonus
+    
+FROM    
+    Employee A
+    
+LEFT JOIN 
+    Bonus B
+    
+    ON A.empId = B.empId
+
+WHERE 
+    B.bonus < 1000 OR B.bonus IS NULL;
